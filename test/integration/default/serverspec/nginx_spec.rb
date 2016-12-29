88e081b3f88970b93338d5e65a07ade64154cfbb
Sebastian Sdorra <s.sdorra@gmail.com>
2016-12-29 16:16:53 +0100

    improve jasas authentication

--- test/integration/default/serverspec/nginx_spec.rb
+++ test/integration/default/serverspec/nginx_spec.rb
@@ -1,8 +1,21 @@
 require 'serverspec'
 require 'spec_helper'
 
+describe 'jasas image exists' do
+  describe docker_image('sdorra/jasas:0.1.0') do
+    it { should exist }
+  end
+end
+
+describe 'jasas container is running' do
+  describe docker_container('auth') do
+    it { should exist }
+    it { should be_running }
+  end
+end
+
 describe 'nginx image exists' do
-  describe docker_image('lebinh/nginx-lua:debian-stock') do
+  describe docker_image('sdorra/nginx:1.11.8') do
     it { should exist }
   end
 end
